86a7b1850e9addb78f806478c3347fa2b50e4d96
Harinath Reddy <harinathareddy7078@gmail.com>
2026-04-22 08:38:37 +0530

    Com

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    background:{
-      default:"#9f68b8"
-    },
-    text:{
-      primary:"#fff"
-    },
-    primary:{
-      main:"rgba(215,106,255,0.507)"
-    }
-  },
-});
+import { darkTheme } from './theme/darktheme';
+import Navbar from './Page/Navbar/Navbar';
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+     <ThemeProvider theme={darkTheme} st>
+
+      <Navbar/>
+
+     </ThemeProvider>
+   
   );
 }
 
--- src/page/Navbar/Navbar.jsx
+++ src/page/Navbar/Navbar.jsx
@@ -1,1 +1,30 @@
-refce
+import React from 'react'
+import Avatar from '@mui/material/Avatar';
+
+const Navbar = () => {
+  return (
+    
+    <div className=' main_container z-10 sticky left-0 right-0 px-5 py-5 lg:px-10 flex justify-between item-center'>
+      <p className='font=bold text-lg'>SPATHA TASK MANAGER</p>
+      <div className='flex ie-center gap'> 
+
+        <div className="logo">
+      <Avatar>7</Avatar>
+      </div>
+      <div  style={{ 
+      backgroundColor: "#2c3e50", 
+      padding: "15px", 
+      color: "white" 
+    }} className='nav_list_ditails'><ul style={{display:'flex', justifyContent:'space-around',listStyle:'none'}}className="nav-links">
+        <li>Home</li>
+        <li>Orders</li>
+        <li>Contact</li>
+      </ul></div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
